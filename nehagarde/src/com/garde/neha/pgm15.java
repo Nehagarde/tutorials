8f1ab7f32a25e14d33cb8e87f06cd8cb0e1e9564
Nehagarde <neha.r.garde@gmail.com>
2014-09-07 16:55:19 +0530

    Update

---
+++ nehagarde/src/com/garde/neha/pgm15.java
@@ -0,0 +1,45 @@
+package com.garde.neha;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class pgm15 extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.cprog1);
+		TextView helloTxt = (TextView)findViewById(R.id.hellotxt);
+        helloTxt.setText(readTxt());
+	
+	}
+	private String readTxt(){
+
+	     InputStream inputStream = getResources().openRawResource(R.raw.cpprog15);
+	     
+	     ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+	     
+	     int i;
+	     try {
+	    	 i = inputStream.read();
+	    	 while (i != -1)
+	    	 {
+	    		 byteArrayOutputStream.write(i);
+	    		 i = inputStream.read();
+	    	 }
+	    	 inputStream.close();
+	     } catch (IOException e) {
+	   // TODO Auto-generated catch block
+	    	 e.printStackTrace();
+	     }
+	  
+	     return byteArrayOutputStream.toString();
+	}
+
+}
